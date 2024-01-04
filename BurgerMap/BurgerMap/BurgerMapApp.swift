1df8b5ec8dd775f946adece0ea657006b2aa454c
masahito-mori <mmori.0426@gmail.com>
2024-01-04 16:05:36 +0900

    📂[プロジェクト作成]新しいプロジェクトを作成した
    【対応内容】
    ・新しいプロジェクトを作成した
    ・gitignoreを追加した
    
    【影響範囲】
    ・プロジェクト
    
    【動作確認】
    ・ビルドできることを確認

---
+++ BurgerMap/BurgerMap/BurgerMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurgerMapApp.swift
+//  BurgerMap
+//
+//  Created by Masahito Mori on 2024/01/04.
+//
+
+import SwiftUI
+
+@main
+struct BurgerMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BurgerMap/BurgerMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BurgerMap
+//
+//  Created by Masahito Mori on 2024/01/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
