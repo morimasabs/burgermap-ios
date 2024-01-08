09d4ba4206959e47ed27960e1235a0e4578a24c8
masahito-mori <mmori.0426@gmail.com>
2024-01-08 11:37:57 +0900

    👍[地図]店のmodelとモックデータを作成し、地図上にマーカーを表示した
    【対応内容】
    ・店のmodelを作成した
    ・店（Shop型）のモックデータを作成した
    ・地図上にマーカーを表示した
    
    【影響範囲】
    ・地図
    
    【動作確認】
    ・地図上に5つのマーカーが表示される

--- BurgerMap/BurgerMap/MapView.swift
+++ BurgerMap/BurgerMap/MapView.swift
@@ -11,14 +11,16 @@ import MapKit
 struct MapView: View {
     private let cameraPosition: MapCameraPosition = MapCameraPosition.camera(MapCamera(centerCoordinate: CLLocationCoordinate2D(latitude: 35.6581, longitude: 139.7017), distance: 2000))
     
-    private let coordinate = CLLocationCoordinate2D(latitude: 35.6581, longitude: 139.7017)
+    private let shops: [Shop] = [.mock1, .mock2, .mock3, .mock4, .mock5]
     
     var body: some View {
         Map(initialPosition: cameraPosition) {
-            Annotation("", coordinate: coordinate) {
-                Image("burger")
+            ForEach(shops) { shop in
+                Annotation("", coordinate: shop.coordinate) {
+                    Image("burger")
+                }
+                .annotationTitles(.hidden)
             }
-            .annotationTitles(.hidden)
         }
     }
 }
