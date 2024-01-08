e5c0f491a40031746b1a3d3e01a6b7cb1524a5b1
masahito-mori <mmori.0426@gmail.com>
2024-01-08 11:12:20 +0900

    👍[地図]地図で渋谷駅に、バーガーのマーカーを表示させた
    【対応内容】
    ・バーガーの画像を追加した
    ・地図上で渋谷駅に、バーガーのマーカー画像を表示させた
    ・マーカー下に表示されるタイトルを非表示にした
    
    【影響範囲】
    ・地図
    
    【動作確認】
    ・地図上で渋谷駅に、バーガーのマーカーが表示される

--- BurgerMap/BurgerMap/MapView.swift
+++ BurgerMap/BurgerMap/MapView.swift
@@ -11,8 +11,15 @@ import MapKit
 struct MapView: View {
     private let cameraPosition: MapCameraPosition = MapCameraPosition.camera(MapCamera(centerCoordinate: CLLocationCoordinate2D(latitude: 35.6581, longitude: 139.7017), distance: 2000))
     
+    private let coordinate = CLLocationCoordinate2D(latitude: 35.6581, longitude: 139.7017)
+    
     var body: some View {
-        Map(initialPosition: cameraPosition)
+        Map(initialPosition: cameraPosition) {
+            Annotation("", coordinate: coordinate) {
+                Image("burger")
+            }
+            .annotationTitles(.hidden)
+        }
     }
 }
 
