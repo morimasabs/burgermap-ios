4b52880ab9e11c44f9f8096bcf8bff409edeb0ed
masahito-mori <mmori.0426@gmail.com>
2024-01-21 17:51:29 +0900

    🎨[ボトムシート]ボトムシートのレイアウトを調整した
    【対応内容】
    ・ボトムシートのレイアウトを調整した
    
    【影響範囲】
    ・ボトムシート
    
    【動作確認】
    ・ボトムシートのレイアウトが調整されていることを確認

--- BurgerMap/BurgerMap/Views/BottomSheetView.swift
+++ BurgerMap/BurgerMap/Views/BottomSheetView.swift
@@ -15,23 +15,42 @@ struct BottomSheetView: View {
             HStack {
                 Image("burger")
                 Text(shop.name)
-                    .font(.system(size: 24))
+                    .font(.system(size: 20))
                     .fontWeight(.heavy)
             }
             .frame(maxWidth: .infinity, alignment: .leading)
-            Text("住所：\(shop.address)")
-                .font(.system(size: 16))
-                .fontWeight(.regular)
-            Text("営業時間：\(shop.open)")
-                .font(.system(size: 16))
-                .fontWeight(.regular)
-            Text("定休日：\(shop.close)")
-                .font(.system(size: 16))
-                .fontWeight(.regular)
+            SectionView(title: "住所")
+            InSectionView(description: shop.address)
+            SectionView(title: "営業時間")
+            InSectionView(description: shop.open)
+            SectionView(title: "定休日")
+            InSectionView(description: shop.close)
         }
         .padding(.horizontal, 20)
         .padding(.top, 16)
         Spacer()
+    }
+    
+    struct SectionView: View {
+        let title: String
+        var body: some View {
+            Text(title)
+                .font(.system(size: 20))
+                .fontWeight(.bold)
+                .padding(.leading, 4)
+                .padding(.bottom, 4)
+        }
+    }
+    
+    struct InSectionView: View {
+        let description: String
+        var body: some View {
+            Text(description)
+                .font(.system(size: 16))
+                .fontWeight(.regular)
+                .padding(.leading, 4)
+                .padding(.bottom, 4)
+        }
     }
 }
 
