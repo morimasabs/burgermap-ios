f109094addca352cc6791e1380c69c3d46cea181
masahito-mori <mmori.0426@gmail.com>
2024-01-27 21:37:09 +0900

    👍[お気に入り]お気に入りの店一覧を表示させた
    【対応内容】
    ・お気に入りの店一覧を表示させた
    
    【影響範囲】
    ・お気に入り
    
    【動作確認】
    ・お気に入りの店一覧が表示される

--- BurgerMap/BurgerMap/Views/FavoriteShops/FavoriteShopsView.swift
+++ BurgerMap/BurgerMap/Views/FavoriteShops/FavoriteShopsView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct FavoriteShopsView: View {
+    var shops: [Shop] = [.mock1, .mock2, .mock3, .mock4, .mock5]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(shops) { shop in
+                HStack {
+                    Image("burger")
+                    VStack(alignment: .leading) {
+                        Text(shop.name)
+                        Text(shop.address)
+                    }
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("お気に入り")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
