5d267bfac2636055243060927ef5be2d22a014f6
masahito-mori <mmori.0426@gmail.com>
2024-03-09 10:46:52 +0900

    ♻️[全体]アプリのエントリーポイントのViewの命名を変更した。
    【対応内容】
    ・アプリのエントリーポイントのViewの命名を変更した
    ・TOP画面をViewに切り出した。
    
    【影響範囲】
    ・全体
    
    【動作確認】
    ・ビルドできることを確認

--- BurgerMap/BurgerMap/BurgerMapApp.swift
+++ BurgerMap/BurgerMap/BurgerMapApp.swift
@@ -16,7 +16,7 @@ struct BurgerMapApp: App {
     
     var body: some Scene {
         WindowGroup {
-            MainView()
+            ContentView()
                 .environmentObject(authViewModel)
         }
     }
--- BurgerMap/BurgerMap/Views/MainView.swift
+++ BurgerMap/BurgerMap/Views/MainView.swift
@@ -2,46 +2,39 @@
 //  MainView.swift
 //  BurgerMap
 //
-//  Created by Masahito Mori on 2024/01/04.
+//  Created by Masahito Mori on 2024/03/03.
 //
 
 import SwiftUI
 
 struct MainView: View {
-    @EnvironmentObject var viewModel: AuthViewModel
-    
     var body: some View {
-        if viewModel.userSession != nil {
-            TabView {
-                MapView()
-                    .tabItem {
-                        VStack {
-                            Image(systemName: "map.fill")
-                            Text("地図")
-                        }
+        TabView {
+            MapView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "map.fill")
+                        Text("地図")
                     }
-                FavoriteShopsView()
-                    .tabItem {
-                        VStack {
-                            Image(systemName: "heart.fill")
-                            Text("お気に入り")
-                        }
+                }
+            FavoriteShopsView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "heart.fill")
+                        Text("お気に入り")
                     }
-                ProfileView()
-                    .tabItem {
-                        VStack {
-                            Image(systemName: "person.fill")
-                            Text("プロフィール")
-                        }
+                }
+            ProfileView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "person.fill")
+                        Text("プロフィール")
                     }
-            }
-        } else {
-            SignInView()
+                }
         }
     }
 }
 
 #Preview {
     MainView()
-        .environmentObject(AuthViewModel())
 }
